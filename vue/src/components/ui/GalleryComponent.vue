537a746dce85ea45d492b451be57018a94c61cc8
Sofia <indecenet@gmail.com>
2024-04-03 23:25:51 +0300

    Edited image object to index, added BEM

--- vue/src/components/ui/GalleryComponent.vue
+++ vue/src/components/ui/GalleryComponent.vue
@@ -1,35 +1,35 @@
 <template>
   <div class="gallery">
-    <div class="selected">
+    <div class="gallery__selected">
       <img 
-        class="selected__image" 
-        :src="selectedImage.url" 
-        :alt="selectedImage.title" 
+        class="gallery__selected__image" 
+        :src="images[selectedImage].url" 
+        :alt="images[selectedImage].title" 
       />
-      <h3>{{ selectedImage.title }}</h3>
-      <p>{{ selectedImage.description }}</p>
+      <h3>{{ images[selectedImage].title }}</h3>
+      <p>{{ images[selectedImage].description }}</p>
     </div>
     <div class="carousel">
       <div 
-        class="arrow" 
+        class="carousel__arrow" 
         @click="prevImage" 
         v-show="images.length > 1"
       >
         &lt;
       </div>
       <div 
-        class="image-container" 
-        v-for="image in images" 
+        class="carousel__image-container" 
+        v-for="(image, index) in images" 
         :key="image.id" 
-        @click="updateSelectedImage(image)"
+        @click="updateSelectedImage(index)"
       >
         <img 
-          class="image-container__image" 
+          class="carousel__image-container__image" 
           :src="image.url" :alt="image.title" 
-          :class = "{'image-container__image__active' : image === selectedImage}" />
+          :class = "{'carousel__image-container__image__active' : image === images[selectedImage]}" />
       </div>
       <div 
-        class="arrow" 
+        class="carousel__arrow" 
         @click="nextImage" 
         v-show="images.length > 1"
       >
@@ -47,30 +47,25 @@ export default {
   },
   data() {
     return {
-      selectedImage: null
+      selectedImage: 0
     };
   },
-  mounted() {
-    this.selectedImage = this.images[0]
-  },
   methods: {
-    updateSelectedImage(image) {
-      this.selectedImage = image
+    updateSelectedImage(index) {
+      this.selectedImage = index
     },
     prevImage() {
-      const index = this.images.indexOf(this.selectedImage)
-      if (index > 0) {
-        this.selectedImage = this.images[index - 1]
+      if (this.selectedImage > 0) {
+        this.selectedImage = this.selectedImage - 1
       } else {
-        this.selectedImage = this.images[this.images.length - 1]
+        this.selectedImage = this.images.length - 1
       }
     },
     nextImage() {
-      const index = this.images.indexOf(this.selectedImage)
-      if (index < this.images.length - 1) {
-        this.selectedImage = this.images[index + 1]
+      if (this.selectedImage < this.images.length - 1) {
+        this.selectedImage = this.selectedImage + 1
       } else {
-        this.selectedImage = this.images[0]
+        this.selectedImage = 0
       }
     }
   }
@@ -86,14 +81,23 @@ export default {
   height: 100vh;
   box-sizing: border-box;
   padding: 20px;
-}
+
+  &__selected {
+    text-align: center;
   
-.selected {
-  text-align: center;
-  
-  &__image {
-    max-width: 100%;
-    max-height: 70vh;
+    &__image {
+      max-width: 100%;
+      max-height: 70vh;
+    }
+  }
+
+  &__carousel {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 20px;
+    
+
   }
 }
   
@@ -102,28 +106,28 @@ export default {
   align-items: center;
   justify-content: center;
   margin-top: 20px;
-}
+
+  &__image-container {
+    margin: 0 5px;
+    cursor: pointer;
   
-.image-container {
-  margin: 0 5px;
-  cursor: pointer;
+    &__image {
+      max-width: 100%;
+      max-height: 50vh;
   
-  &__image {
-    max-width: 100%;
-    max-height: 50vh;
-  
-    &__active {
-      transform: scale(1.1);
+      &__active {
+        transform: scale(1.1);
+      }
     }
   }
-}
- 
-.arrow {
-  background-color: #ccc;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
+
+  &__arrow {
+    background-color: #ccc;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+  }
 }
 </style>
